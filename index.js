44822a17083b2d7ba0a12c82ba380db3e800a776
Casey Ocampo <caseysoflo@gmail.com>
2022-02-07 16:00:43 -0500

    Add birbs

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ function getRandomActivity() {
             document.getElementById(
                 'activityMessage'
             ).innerHTML = `✨<span class="activityGlam">&nbsp;${data.activity}&nbsp;</span>✨`
+
+            displayBirb()
         })
 }
 
 const randomActivityBtn = document.getElementById('randomActivityBtn')
 randomActivityBtn.addEventListener('click', getRandomActivity)
+
+function displayBirb() {
+    let birbs = document.getElementsByClassName('birb')
+    for (let birb of birbs) {
+        birb.style.display = 'block'
+    }
+}
